d7678038b67a628da660b1394f7934b2a0b5fcae
Spiri0 <69024222+Spiri0@users.noreply.github.com>
2024-04-17 04:47:54 +0200

    Update setStorageTexture.js

--- resources/shader/IFFT/setStorageTexture.js
+++ resources/shader/IFFT/setStorageTexture.js
@@ -5,16 +5,16 @@ export const SetStorageTextureWGSL = wgslFn(`
 
     fn computeWGSL( 
         writeTex: texture_storage_2d<rgba32float, write>,
-	    readTex: texture_2d<f32>,
+	readTex: texture_2d<f32>,
         index: u32,
         size: f32,
     ) -> void {
 
-        var posX = f32(index) % size;
-        var posY = floor(f32(index) / size);
-        var idx = vec2i(i32(posX), i32(posY));
+        var posX = index % u32(size);
+        var posY = index / u32(size);
+        var idx = vec2u(posX, posY);
 
-	    var texture = textureLoad(readTex, idx, 0);
+	var texture = textureLoad(readTex, idx, 0);
         textureStore(writeTex, idx, texture);
     }
 `);
