e953ac61f58d9f6c6ab8d5370215faad67493e60
Spiri0 <69024222+Spiri0@users.noreply.github.com>
2024-04-26 08:48:54 +0200

    Update ocean-builder-threaded-worker.js

--- src/ocean/ocean-builder-threaded-worker.js
+++ src/ocean/ocean-builder-threaded-worker.js
@@ -36,22 +36,22 @@ class OceanBuilderThreadedWorker {
 	Build() {
 		const positions = [];
 		const vindices = [];
-        const widths = [];
-        const lods = [];
+        	const widths = [];
+        	const lods = [];
 
 		const localToWorld = this.params_.worldMatrix;
 		const resolution = this.params_.resolution;
 		const offset = this.params_.offset;
 		const width = this.params_.width;
 		const half = width / 2;
-        const lod = this.params_.lod;
+        	const lod = this.params_.lod;
 
 		let idx = 0;
 
-        for (let x = 0; x < resolution + 1; x++) {
-            const xp = width * x / resolution;
-            for (let y = 0; y < resolution + 1; y++) {
-                const yp = width * y / resolution;
+        	for (let x = 0; x < resolution + 1; x++) {
+            		const xp = width * x / resolution;
+            		for (let y = 0; y < resolution + 1; y++) {
+                		const yp = width * y / resolution;
 
 				// Compute position
 				_P.set(xp - half, yp - half, 0);
@@ -60,8 +60,8 @@ class OceanBuilderThreadedWorker {
 
 				positions.push(_P.x, _P.y, _P.z);
 				vindices.push(idx);
-                widths.push(width);
-                lods.push(lod);
+                		widths.push(width);
+                		lods.push(lod);
 				idx += 1;				
 			}
 		}
@@ -70,24 +70,33 @@ class OceanBuilderThreadedWorker {
 		const bytesInFloat32 = 4;
 		const bytesInInt32 = 4;
 		
+		/*
 		const positionsArray = new Float32Array(new SharedArrayBuffer(bytesInFloat32 * positions.length));
-        const widthArray = new Float32Array(new SharedArrayBuffer(bytesInFloat32 * vindices.length));
+        	const widthArray = new Float32Array(new SharedArrayBuffer(bytesInFloat32 * vindices.length));
 		const vindicesArray = new Uint32Array(new SharedArrayBuffer(bytesInInt32 * vindices.length));
 		const indicesArray = new Uint32Array(new SharedArrayBuffer(bytesInInt32 * indices.length));
-        const lodArray = new Uint32Array(new SharedArrayBuffer(bytesInInt32 * vindices.length));
+        	const lodArray = new Uint32Array(new SharedArrayBuffer(bytesInInt32 * vindices.length));
+		*/
+		
+		const positionsArray = new Float32Array(new ArrayBuffer(bytesInFloat32 * positions.length));
+        	const widthArray = new Float32Array(new ArrayBuffer(bytesInFloat32 * vindices.length));
+		const vindicesArray = new Uint32Array(new ArrayBuffer(bytesInInt32 * vindices.length));
+		const indicesArray = new Uint32Array(new ArrayBuffer(bytesInInt32 * indices.length));
+        	const lodArray = new Uint32Array(new ArrayBuffer(bytesInInt32 * vindices.length));
 
+		
 		positionsArray.set(positions, 0);
 		vindicesArray.set(vindices, 0);
 		indicesArray.set(indices, 0);
-        widthArray.set(widths, 0);
-        lodArray.set(lods, 0);
+        	widthArray.set(widths, 0);
+        	lodArray.set(lods, 0);
 
 		return {
 			positions: positionsArray,
 			vindices: vindicesArray,
 			indices: indicesArray,
-            width: widthArray,
-            lod: lodArray
+            		width: widthArray,
+            		lod: lodArray
 		};
 	}
 }//end class
