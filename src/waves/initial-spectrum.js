5c75305822112fb26e4189533c1fc0c6a365f379
Spiri0 <69024222+Spiri0@users.noreply.github.com>
2024-04-27 07:34:19 +0200

    Update initial-spectrum.js
    cleaned up

--- src/waves/initial-spectrum.js
+++ src/waves/initial-spectrum.js
@@ -8,73 +8,72 @@ import {wave_constants} from './wave-constants.js';
 
 export const initial_spectrum = (() => {
 
-    class InitialSpectrum extends entity.Component {
+	class InitialSpectrum extends entity.Component {
 		constructor(params) {
 			super();
 			this.Init(params);
-        }
+		}
 
-        Init(params) {
-            this.params_ = params;
+		Init(params) {
+			this.params_ = params;
 
 
-            this.defaultWorkgroup = wave_constants.DEFAULT_WORKGROUP;
+			this.defaultWorkgroup = wave_constants.DEFAULT_WORKGROUP;
 
-            this.spectrumTexture = new StorageTexture(params.size, params.size);    
-            this.waveDataTexture = new StorageTexture(params.size, params.size);
-            this.h0k_Texture = new StorageTexture(params.size, params.size); 
-            this.spectrumTexture.magFilter = this.spectrumTexture.minFilter = THREE.NearestFilter;
-            this.waveDataTexture.magFilter = this.waveDataTexture.minFilter = THREE.NearestFilter;
-            this.h0k_Texture.magFilter = this.h0k_Texture.minFilter = THREE.NearestFilter;
-            this.spectrumTexture.type = THREE.FloatType;
-            this.waveDataTexture.type = THREE.FloatType;
-            this.h0k_Texture.type = THREE.FloatType;
+			this.spectrumTexture = new StorageTexture(params.size, params.size);    
+			this.waveDataTexture = new StorageTexture(params.size, params.size);
+			this.h0k_Texture = new StorageTexture(params.size, params.size); 
+			this.spectrumTexture.magFilter = this.spectrumTexture.minFilter = THREE.NearestFilter;
+			this.waveDataTexture.magFilter = this.waveDataTexture.minFilter = THREE.NearestFilter;
+			this.h0k_Texture.magFilter = this.h0k_Texture.minFilter = THREE.NearestFilter;
+			this.spectrumTexture.type = THREE.FloatType;
+			this.waveDataTexture.type = THREE.FloatType;
+			this.h0k_Texture.type = THREE.FloatType;
             
 
-            this.InitialSpectrum(params);
-            this.InitialSpectrumWithInverse(params);
+			this.InitialSpectrum(params);
+			this.InitialSpectrumWithInverse(params);
 
-        }
+		}
 
  
-        InitialSpectrum(params){
+		InitialSpectrum(params){
 
-            const initialSpectrum = InitialSpectrumWGSL({ 
-                writeSpectrum: textureStore(this.spectrumTexture),
-                writeWaveData: textureStore(this.waveDataTexture),
-                index: instanceIndex,
-                size: params.size,
-                waveLength: uniform(params.lengthScale),
-                boundaryLow: uniform(params.boundaryLow),
-                boundaryHigh: uniform(params.boundaryHigh),
-                //can be made better in the future with structs in the shader
-                ...params.waveSettings
-            }).compute(params.size ** 2);
-            params.renderer.compute(initialSpectrum, this.defaultWorkgroup);
-        }
+			const initialSpectrum = InitialSpectrumWGSL({ 
+				writeSpectrum: textureStore(this.spectrumTexture),
+				writeWaveData: textureStore(this.waveDataTexture),
+				index: instanceIndex,
+				size: params.size,
+				waveLength: uniform(params.lengthScale),
+				boundaryLow: uniform(params.boundaryLow),
+				boundaryHigh: uniform(params.boundaryHigh),
+				...params.waveSettings
+			}).compute(params.size ** 2);
+			params.renderer.compute(initialSpectrum, this.defaultWorkgroup);
+		}
         
 
-        InitialSpectrumWithInverse(params){
+		InitialSpectrumWithInverse(params){
 
-            const initialSpectrumWithInverse = InitialSpectrumWithInverseWGSL({ 
-                writeSpectrum: textureStore(this.h0k_Texture),
-                readSpectrum: texture(this.spectrumTexture),
-                index: instanceIndex,
-                size: params.size,
-            }).compute(params.size ** 2);
-            params.renderer.compute(initialSpectrumWithInverse, this.defaultWorkgroup);
-        }
+			const initialSpectrumWithInverse = InitialSpectrumWithInverseWGSL({ 
+				writeSpectrum: textureStore(this.h0k_Texture),
+				readSpectrum: texture(this.spectrumTexture),
+				index: instanceIndex,
+				size: params.size,
+			}).compute(params.size ** 2);
+			params.renderer.compute(initialSpectrumWithInverse, this.defaultWorkgroup);
+		}
 
 
-        Update(_) {
+		Update(_) {
 
-        }
+		}
 
-    }
+	}
 
-    return {
-        InitialSpectrum: InitialSpectrum,
-    };
+	return {
+		InitialSpectrum: InitialSpectrum,
+	};
   
 })();
 
