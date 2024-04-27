ac90bcd8692fffd20eeb94ce0d191967f4a64c97
Spiri0 <69024222+Spiri0@users.noreply.github.com>
2024-04-27 09:01:18 +0200

    Update ocean-constants.js
    add comment

--- src/ocean/ocean-constants.js
+++ src/ocean/ocean-constants.js
@@ -5,7 +5,8 @@ export const ocean_constants = (() => {
   	return {
 		QT_OCEAN_MIN_LOD_RADIUS: 15,
 		QT_OCEAN_MIN_NUM_LAYERS: 15,	
-		QT_OCEAN_MIN_CELL_RESOLUTION: 12,
+		QT_OCEAN_MIN_CELL_RESOLUTION: 12,	//even numbers only
 		OCEAN_SIZE: 500000,
   	}
+	
 })();
