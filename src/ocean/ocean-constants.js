5161f33ce4317c2ae39da975d34eb723b7478871
Spiri0 <69024222+Spiri0@users.noreply.github.com>
2024-04-28 06:20:20 +0200

    Update ocean-constants.js
    increase wireframe resolution because of reduced quadtree subdivision

--- src/ocean/ocean-constants.js
+++ src/ocean/ocean-constants.js
@@ -5,7 +5,7 @@ export const ocean_constants = (() => {
   	return {
 		QT_OCEAN_MIN_LOD_RADIUS: 15,
 		QT_OCEAN_MIN_NUM_LAYERS: 15,	
-		QT_OCEAN_MIN_CELL_RESOLUTION: 24,	//even numbers only
+		QT_OCEAN_MIN_CELL_RESOLUTION: 36,	//even numbers only
 		OCEAN_SIZE: 500000,
   	}
 	
